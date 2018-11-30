336c2b21f70fbd61d91fad8fbad5b056ef4f28b7
MR.Zhou <saw71@outlook.com>
2018-11-30 10:48:52 +0800

    commit222

---
+++ 1130New/ShowLog.h
@@ -0,0 +1,17 @@
+//
+//  ShowLog.h
+//  1130New
+//
+//  Created by Apple on 2018/11/30.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ShowLog : NSObject
++(void)showLog;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 1130New/ShowLog.m
@@ -0,0 +1,15 @@
+//
+//  ShowLog.m
+//  1130New
+//
+//  Created by Apple on 2018/11/30.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+#import "ShowLog.h"
+
+@implementation ShowLog
++(void)showLog{
+    NSLog(@"这是代码夸啊");
+}
+@end
